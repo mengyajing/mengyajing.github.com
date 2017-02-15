d1ec35e3419ec8b1d3808ce5c8be2eb151d5e26f
mengyajing <1359264656@qq.com>
2017-02-15 21:20:39 +0800

    m
    m

--- Portfolios/豆瓣电影项目/movie/movie-list-controller.js
+++ Portfolios/豆瓣电影项目/movie/movie-list-controller.js
@@ -8,12 +8,6 @@
         var type = $routeParams.type || 'in_theaters';
         var fullUrl = url + type;
         console.log(fullUrl);
-        // $http({
-        // //    请求头 请求参数
-        //     url : fullUrl,
-        //     method : 'GET',
-        //     ...
-        // })
         $scope.loading = true;
         //参数当中必须带有一个callBack函数
         var trueUrl = fullUrl + '?count='+count+'&start='+0+'&callback=movieListCallback';
@@ -22,9 +16,6 @@
         }).success(function (){
             console.log('success',arguments);
         });
-        // function movieListCallback() {
-        //
-        // } 闭包
         window.movieListCallback = function (jsonData){
             console.log(jsonData);
             $scope.title = jsonData.title;
